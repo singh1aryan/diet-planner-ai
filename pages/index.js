ce08a6daffa2532dff220a8859c8162aedf643b8
Aryan Singh <aryansingh@MacBook-Pro.attlocal.net>
2023-07-07 23:52:28 -0700

    setup files

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,115 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useState, useEffect } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+// import { Oval } from 'react-loader-spinner';
 
-export default function Home() {
+const fitness = `
+Act as a fitness expert.
+I will give you my person details in the next message.
+Make a complete diet plan according to my needs.
+Wait for my next message and then return the diet plan.
+`;
+
+export default function GeneratePosts() {
+
+
+  const [content, setContent] = useState("Your diet plan will come here.");
+  const [loading, setLoading] = useState(false);
+
+  async function callGPT(prompt, newContent) {
+    console.log("calling chat gpt");
+
+    // https://api.openai.com/v1/chat/completions
+    setLoading(true);
+    try {
+      const response = await fetch(process.env.chatGpt, {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${process.env.apiKey}`
+        },
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: [{
+            "role": "user",
+            "content": prompt
+          }, {
+            "role": "user",
+            "content": newContent
+          }]
+
+        })
+      });
+
+      const data = await response.json();
+      const maal = data.choices[0].message.content;
+
+      setLoading(false);
+      setContent(maal);
+
+    } catch (error) {
+      console.error(error.message);
+      setLoading(false);
+      setContent("ERROR while generating, check something.");
+    }
+  };
+
+  const handleContentChange = (event) => {
+    setContent(event.target.value);
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+      <div className="flex">
+
+        <main className="w-full p-5">
+          <div className="flex flex-wrap ring-1 ring-gray-200 rounded-3xl">
+            <div className="p-5">
+
+              <label htmlFor="message" className="text-xl block mb-3 text-sm font-medium text-gray-900 dark:text-white">Enter your personal details 👇</label>
+
+            </div>
+
+            <div className="p-5 lg:w-1/2 w-full  max-w-full">
+              <label htmlFor="message" className="block mt-5 text-xl font-medium text-gray-900 dark:text-white">Your diet plan here 👇</label>
+              <div className="pt-3">
+                {
+                  !loading &&
+                  <textarea
+                    value={content}
+                    onChange={handleContentChange}
+                    id="message" rows="30"
+                    className="border border-gray-200 rounded-lg w-full p-2.5 text-sm text-gray-900 "
+                    placeholder="Write your thoughts here...">
+                  </textarea>
+                }
+              </div>
+            </div>
+
+          </div>
+
+        </main>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>);
 }
+
+
+// function Spinner() {
+//     return <Oval
+//         height={40}
+//         width={40}
+//         color="#4fa94d"
+//         wrapperStyle={{}}
+//         wrapperclassName=""
+//         visible={true}
+//         ariaLabel='oval-loading'
+//         secondaryColor="#4fa94d"
+//         strokeWidth={2}
+//         strokeWidthSecondary={2}
+//     />;
+// }
+
+// getstaticprops
+
+// getserversideprops
+
+// /api folder
